a67c84524587a2365a88996c45118b1a0b155589
AnusaraTrokhum <anusara.orm@gmail.com>
2016-08-22 19:20:50 +0700

    edit

---
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/alert/alert1Activity.java
@@ -0,0 +1,13 @@
+package com.example.anusaratrokhum.myapplication.alert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alert1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert1);
+    }
+}
