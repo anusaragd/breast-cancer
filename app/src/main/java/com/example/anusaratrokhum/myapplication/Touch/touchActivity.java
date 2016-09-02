cb170c6b5d12ac49f2cee53afb463a6282e887c2
anusaragd <anusara.orm@gmail.com>
2016-09-03 02:26:07 +0700

    edit

--- app/src/main/java/com/example/anusaratrokhum/myapplication/Touch/touchActivity.java
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/Touch/touchActivity.java
@@ -1,17 +1,63 @@
 package com.example.anusaratrokhum.myapplication.Touch;
 
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.anusaratrokhum.myapplication.R;
 
+import java.util.ArrayList;
+
 public class touchActivity extends AppCompatActivity {
+
+    private GestureLibrary gLib;
+    private static final String TAG = "gesture";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch);
 
+        gLib = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!gLib.load()) {
+            Log.w(TAG, "could not load gesture library");
+            finish();
+        }
+
+        GestureOverlayView gestures = (GestureOverlayView) findViewById(R.id.gestures);
+        gestures.addOnGesturePerformedListener(handleGestureListener);
 
     }
+    /**
+     * our gesture listener
+     */
+    private GestureOverlayView.OnGesturePerformedListener handleGestureListener = new GestureOverlayView.OnGesturePerformedListener() {
+        @Override
+        public void onGesturePerformed(GestureOverlayView gestureView,
+                                       Gesture gesture) {
+
+            ArrayList<Prediction> predictions = gLib.recognize(gesture);
+
+            // one prediction needed
+            if (predictions.size() > 0) {
+                Prediction prediction = predictions.get(0);
+                // checking prediction
+                if (prediction.score > 1.0) {
+                    // and action
+                    Toast.makeText(touchActivity.this,"Correct",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(touchActivity.this,"InCorrect",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    };
 }
