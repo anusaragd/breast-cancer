14deac1b314f4a10626549bced0049df928fd6e0
anusaragd <anusara.orm@gmail.com>
2016-09-15 16:35:02 +0700

    edit

--- app/src/main/java/com/example/anusaratrokhum/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/RegisterActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import okhttp3.RequestBody;
+
 public class RegisterActivity extends AppCompatActivity {
 
 
@@ -15,11 +17,23 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final Button submit = (Button) findViewById(R.id.subbutt);
-        submit.setOnClickListener(new View.OnClickListener() {
+//        final Button submit = (Button) findViewById(R.id.subbutt);
+//        submit.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.subbutt) {
+//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(intent);
+//                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+//                    //startActivity(intent);
+//                }
+//
+//            }
+//        });
+        final Button cancel = (Button) findViewById(R.id.canbutt);
+        cancel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (v.getId() == R.id.subbutt) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                     startActivity(intent);
                     //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
                     //startActivity(intent);
