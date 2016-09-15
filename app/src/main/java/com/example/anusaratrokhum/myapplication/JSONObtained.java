475c6bff6286e7315d54e80af44543afb2f01c51
anusaragd <anusara.orm@gmail.com>
2016-09-15 15:51:27 +0700

    edit  login

---
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/JSONObtained.java
@@ -0,0 +1,52 @@
+package com.example.anusaratrokhum.myapplication;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by anusara on 15-Sep-16.
+ */
+public class JSONObtained {
+//    public static final String BASE_URL = "http://192.168.1.35/breast-cancer/";
+    public static final String BASE_URL = "http://192.168.1.33/breast-cancer/";
+    private static OkHttpClient client = null;
+
+    public static OkHttpClient getInstance() {
+        if (client == null) {
+            client = new OkHttpClient.Builder()
+                    .connectTimeout(20, TimeUnit.SECONDS)// connect timeout
+                    .writeTimeout(200, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS) // socket timeout
+                    .build();
+
+        }
+
+        return client;
+    }
+    public static Request getRequest(HttpUrl url){
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        return request;
+    }
+    public static Request postRequest(HttpUrl url, RequestBody formBody){
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        return request;
+    }
+
+    public static String getAbsoluteUrl(String relativeUrl) {
+
+        return BASE_URL + relativeUrl;
+    }
+
+}
+
--- app/src/main/java/com/example/anusaratrokhum/myapplication/MainActivity.java
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.anusaratrokhum.myapplication;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button signin;
+    private EditText username, password;
 
 
 
@@ -17,44 +33,178 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button signin = (Button) findViewById(R.id.signin);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        bindWidget();
+
+        controlActivity();
+    }
+
+    private void controlActivity() {
+
         signin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.signin) {
-                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                    startActivity(intent);
-                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-                    //startActivity(intent);
+
+              /*  String strUsername = username.getText().toString().trim();
+                String strPassword = password.getText().toString().trim();
+
+                if (strUsername.equals("") || strPassword.equals("")) {
+
+                    Toast.makeText(Login2Activity.this,"กรุณากรอกข้อมูลให้ครบถ้วน", Toast.LENGTH_SHORT).show();
+
+                }*/
+
+                if (checkEmpty()) {
+
+                    LoginData();
+
                 }
 
             }
         });
-        final Button signup = (Button) findViewById(R.id.signup);
-        signup.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (v.getId() == R.id.signup) {
-                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                    startActivity(intent);
-                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-                    //startActivity(intent);
-                }
 
+    }
+    private boolean checkEmpty() {
+
+
+        if (password.getText().length() == 0 && username.getText().length() == 0){
+
+            Toast.makeText(MainActivity.this,"กรุณากรอกชื่อผู้ใช้และรหัสผ่าน", Toast.LENGTH_SHORT).show();
+
+            return false;
+
+        }else if (password.getText().length() == 0) {
+
+            Toast.makeText(MainActivity.this,"กรุณากรอกรหัสผ่าน", Toast.LENGTH_SHORT).show();
+
+            return false;
+
+        }else if (username.getText().length() == 0) {
+
+            Toast.makeText(MainActivity.this,"กรุณากรอกชื่อผู้ใช้", Toast.LENGTH_SHORT).show();
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+    private void bindWidget() {
+
+        username = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        signin = (Button) findViewById(R.id.signin);
+
+    }
+    public void LoginData() {
+        HttpUrl httpUrl = HttpUrl.parse(JSONObtained.BASE_URL + "checklogin.php");
+        RequestBody formBody = new FormBody.Builder()
+                .add("username", username.getText().toString())
+                .add("password", password.getText().toString())
+                .build();
+        JSONObtained.getInstance().newCall(JSONObtained.postRequest(httpUrl, formBody)).enqueue(new Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
-        });
 
-        final Button skip = (Button) findViewById(R.id.skipbut);
-        skip.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (v.getId() == R.id.skipbut) {
-                    Intent intent = new Intent(getApplicationContext(), GuestActivity.class);
-                    startActivity(intent);
-                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-                    //startActivity(intent);
-                }
+            @Override
+            public void onResponse(okhttp3.Call call, Response response) throws IOException {
 
+                final String json = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        Toast.makeText(getApplicationContext(),json,Toast.LENGTH_SHORT).show();
+
+                        String status = null;
+
+                        try {
+                            JSONObject jObj = new JSONObject(json);
+                            status = jObj.getString("StatusID");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        if (status.equals("1")) {
+                            Toast.makeText(getApplicationContext(), "ยินดีต้อนรับ", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+//                        Intent intentObj = new Intent(MainActivity.this,Main2Activity.class);
+                        } else if (status.equals("0")) {
+                            Toast.makeText(getApplicationContext(), "ไม่มีบัญชีผู้ใช้นี้", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+//                        Intent intentObj = new Intent(MainActivity.this,Main2Activity.class);
+                        }
+
+
+                    }
+                });
             }
+
+
         });
     }
+
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+//        final Button signin = (Button) findViewById(R.id.signin);
+//        signin.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.signin) {
+//                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+//                    startActivity(intent);
+//                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+//                    //startActivity(intent);
+//                }
+//
+//            }
+//        });
+//        final Button signup = (Button) findViewById(R.id.signup);
+//        signup.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.signup) {
+//                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+//                    startActivity(intent);
+//                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+//                    //startActivity(intent);
+//                }
+//
+//            }
+//        });
+//
+//        final Button skip = (Button) findViewById(R.id.skipbut);
+//        skip.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if (v.getId() == R.id.skipbut) {
+//                    Intent intent = new Intent(getApplicationContext(), GuestActivity.class);
+//                    startActivity(intent);
+//                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
+//                    //startActivity(intent);
+//                }
+//
+//            }
+//        });
+//    }
 
 //    public void onClickNext(View view){
 //        Button skipbut = (Button)findViewById(R.id.skipbut);
