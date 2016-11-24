dfde4a51283d5c430f005d3d2f97bacaff537c63
anusaragd <anusara.orm@gmail.com>
2016-11-24 21:29:05 +0700

    register

--- app/src/main/java/com/example/anusaratrokhum/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/anusaratrokhum/myapplication/RegisterActivity.java
@@ -63,9 +63,12 @@ public class RegisterActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
                 if (checkFields()) {
                     saveData();
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(intent);
                 }
             }
         });
@@ -175,14 +178,6 @@ public class RegisterActivity extends AppCompatActivity {
             txtPassword.requestFocus();
             return false;
         }
-//        // Check Password and Confirm Password (Match)
-//        if(!txtPassword.getText().toString().equals(txtConPassword.getText().toString()))
-//        {
-//            ad.setMessage("Password and Confirm Password Not Match! ");
-//            ad.show();
-//            txtConPassword.requestFocus();
-//            return false;
-//        }
         // Check Name
         if (txtName.getText().length() == 0) {
             ad.setMessage("Please input [Name] ");
@@ -257,7 +252,8 @@ public class RegisterActivity extends AppCompatActivity {
                 .addFormDataPart("password", txtPassword.getText().toString())
                 .addFormDataPart("email", txtEmail.getText().toString())
                 .addFormDataPart("name", txtName.getText().toString())
-                .addFormDataPart("lastname", txtlastName.getText().toString())*/
+                .addFormDataPart("lastname", txtlastName.getText().toString())
+                .build();*/
         String run(String url) throws IOException {
             Request request = new Request.Builder()
                     .post(requestBody)
@@ -269,225 +265,6 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 }
-
-
-//
-//   // private TextView tvDate;
-//    Button Button1;
-//
-//    EditText txtUsername , txtPassword, txtEmail, txtName, txtlastName;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_register);
-//        inits();
-//
-////
-////
-////        final Button submit = (Button) findViewById(R.id.subbutt);
-////        submit.setOnClickListener(new View.OnClickListener() {
-////            public void onClick(View v) {
-////                if (v.getId() == R.id.subbutt) {
-////                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-////                    startActivity(intent);
-////                    //Intent intent = new Intent(getApplicationContext(), homeActivity.class);
-////                    //startActivity(intent);
-////                }
-////
-////            }
-////        });
-//
-//
-//        // btnSave
-//         /*Button btnSave = (Button) findViewById(R.id.subbutt);
-//        // Perform action on click
-//        btnSave.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (checkFields()) {
-//                    saveData();
-//                }
-//            }
-//        });
-//        final Button cancel = (Button) findViewById(R.id.canbutt);
-//        assert cancel != null;
-//        cancel.setOnClickListener(new View.OnClickListener() {
-//                                      @Override
-//                                      public void onClick(View v) {
-//                                          Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-//                                          startActivity(intent);
-//                                      }
-//                                  });
-//
-//                //birthday();
-//*/
-//    }
-//
-//    // Get Current Date
-//    public void inits(){
-//        //tvDate = (TextView) findViewById(R.id.tv_date);
-//        txtUsername = (EditText) findViewById(R.id.username);
-//        txtPassword = (EditText) findViewById(R.id.password);
-//        txtEmail = (EditText) findViewById(R.id.email);
-//        txtName = (EditText) findViewById(R.id.name);
-//        txtlastName = (EditText) findViewById(R.id.lastname);
-//        Button1 = (Button) findViewById(R.id.button);
-//        Button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new AsyncTask<Void, Void, String>() {
-//                    @Override
-//                    protected String doInBackground(Void... voids) {
-//                        getHttp http = new getHttp();
-//                        String response = null;
-//                        try {
-//                            response = http.run("http://localhost/breast-cancer/register2.php");
-//                        } catch (IOException e) {
-//                            // TODO Auto-generated catch block
-//                            e.printStackTrace();
-//                        }
-//                        return response;
-//                    }
-//
-//                    @Override
-//                    protected void onPostExecute(String string) {
-//                        super.onPostExecute(string);
-//
-//                        Log.e( "onPostExecute: ",string + "GG");
-//
-//
-//                /*try {
-//                    JSONObject gg = new JSONObject(string);
-//                    Log.e( "onPostExecute: ", gg.getString("StatusIDStatusID" ));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//*/
-//
-//                    }
-//                }.execute();
-//            }
-//        });
-//    }
-//
-//    public boolean checkFields() {
-//
-//
-//        // Dialog
-//        final AlertDialog.Builder ad = new AlertDialog.Builder(this);
-//
-//        ad.setTitle("Error! ");
-//        ad.setIcon(android.R.drawable.btn_star_big_on);
-//        ad.setPositiveButton("Close", null);
-//
-//        // Check Username
-//        if (txtUsername.getText().length() == 0) {
-//            ad.setMessage("Please input [Username] ");
-//            ad.show();
-//            txtUsername.requestFocus();
-//            return false;
-//        }
-//        // Check Password
-//        if (txtPassword.getText().length() == 0) {
-//            ad.setMessage("Please input [Password ");
-//            ad.show();
-//            txtPassword.requestFocus();
-//            return false;
-//        }
-////        // Check Password and Confirm Password (Match)
-////        if(!txtPassword.getText().toString().equals(txtConPassword.getText().toString()))
-////        {
-////            ad.setMessage("Password and Confirm Password Not Match! ");
-////            ad.show();
-////            txtConPassword.requestFocus();
-////            return false;
-////        }
-//        // Check Name
-//        if (txtName.getText().length() == 0) {
-//            ad.setMessage("Please input [Name] ");
-//            ad.show();
-//            txtName.requestFocus();
-//            return false;
-//        }
-//        // Check Email
-//        if (txtEmail.getText().length() == 0) {
-//            ad.setMessage("Please input [Email] ");
-//            ad.show();
-//            txtEmail.requestFocus();
-//            return false;
-//        }
-//        // Check Tel
-//        if (txtlastName.getText().length() == 0) {
-//            ad.setMessage("Please input [LastName] ");
-//            ad.show();
-//            txtlastName.requestFocus();
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    private void saveData() {
-//        new AsyncTask<Void, Void, String>() {
-//            @Override
-//            protected String doInBackground(Void... voids) {
-//                getHttp http = new getHttp();
-//                String response = null;
-//                try {
-//                    response = http.run("http://localhost/breast-cancer/register2.php");
-//                } catch (IOException e) {
-//                    // TODO Auto-generated catch block
-//                    e.printStackTrace();
-//                }
-//                return response;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(String string) {
-//                super.onPostExecute(string);
-//
-//                Log.e( "onPostExecute: ",string + "GG");
-//
-//
-//                /*try {
-//                    JSONObject gg = new JSONObject(string);
-//                    Log.e( "onPostExecute: ", gg.getString("StatusIDStatusID" ));
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//*/
-//
-//            }
-//        }.execute();
-//    }
-//
-//    public class getHttp {
-//        OkHttpClient client = new OkHttpClient();
-//        RequestBody requestBody = new MultipartBody.Builder()
-//                .setType(MultipartBody.FORM)
-//                .addFormDataPart("username", "sss")
-//                .addFormDataPart("password", "sss")
-//                .addFormDataPart("email", "sss")
-//                .addFormDataPart("name", "sss")
-//                .addFormDataPart("lastname", "sss")
-//                .build();
-//        /*.addFormDataPart("username", txtUsername.getText().toString())
-//                .addFormDataPart("password", txtPassword.getText().toString())
-//                .addFormDataPart("email", txtEmail.getText().toString())
-//                .addFormDataPart("name", txtName.getText().toString())
-//                .addFormDataPart("lastname", txtlastName.getText().toString())*/
-//        String run(String url) throws IOException {
-//            Request request = new Request.Builder()
-//                    .post(requestBody)
-//                    .url(url)
-//                    .build();
-//            Response response = client.newCall(request).execute();
-//            return response.body().string();
-//        }
-//    }
-//}
-//
-
 
 
 
